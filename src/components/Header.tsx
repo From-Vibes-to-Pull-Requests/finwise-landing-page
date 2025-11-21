938b5855f4af93ff1d815e189c21f01850ea69b1
M1KE-M0RALES <133127935+M1KE-M0RALES@users.noreply.github.com>
2025-11-21 14:44:58 +0100

    Download btn update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -39,9 +39,12 @@ const Header: React.FC = () => {
                             </li>
                         ))}
                         <li>
-                            <Link href="#cta" className="text-black bg-primary hover:bg-primary-accent px-8 py-3 rounded-full transition-colors">
-                                Download
-                            </Link>
+                            <Link
+                                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1"
+                                className="text-black bg-primary hover:bg-primary-accent px-8 py-3 rounded-full transition-colors"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
                         </li>
                     </ul>
 
