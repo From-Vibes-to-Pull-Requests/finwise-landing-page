1f8b22a6a32cd0b2bd54be6096c79bb76023b907
Michael Morales <michael@example.com>
2025-11-21 13:44:20 +0100

    Revised Pricing number

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,5 +1,7 @@
 export const heroDetails = {
-    heading: 'Best App in the World',
+
+    heading: 'You are here already, just buy it.',
+
     subheading: 'From effortless budgeting to real-time investment insights, Finwise puts you in control of your money like never before',
     centerImageSrc: '/images/hero-mockup.webp',
 }
